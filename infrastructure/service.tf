15646d6b3cadedb7b238c5b966c27ec1f94d631c
neeraj <kansalneeraj9194@gmail.com>
2021-07-16 17:37:22 +0530

    removed network interface

--- infrastructure/service.tf
+++ infrastructure/service.tf
@@ -20,29 +20,14 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-
-resource "aws_network_interface" "service-box" {
-  count     = local.instance_count
-  subnet_id = count.index == 0 || count.index == 1 ? aws_subnet.private[0].id : aws_subnet.private[count.index - 1].id
-  security_groups = [aws_security_group.private_instance_sg.id]
-  tags = {
-    Name = "ec2_network_interface-${count.index}"
-  }
-}
-
-
 resource "aws_instance" "service-box" {
   count           = local.instance_count
   ami             = data.aws_ami.ubuntu.id
   key_name        = aws_key_pair.awskeypair.key_name
 
   instance_type = "t2.micro"
-
-  network_interface {
-
-    network_interface_id = aws_network_interface.service-box[count.index].id
-    device_index         = 0
-  }
+  subnet_id = count.index == 0 || count.index == 1 ? aws_subnet.private[0].id : aws_subnet.private[count.index - 1].id
+  vpc_security_group_ids = [aws_security_group.private_instance_sg.id]
 
   tags = {
     Name = "service-box-${count.index}"
@@ -70,27 +55,15 @@ resource "aws_volume_attachment" "service_box_attach" {
 
 # Bastion Host
 
-resource "aws_network_interface" "bastion-host" {
-  count     = var.availability_zones_count
-  subnet_id = aws_subnet.public[count.index].id
-  security_groups = [aws_security_group.bastion_sg.id]
-  tags = {
-    Name = "bastion_network_interface-${count.index}"
-  }
-}
 resource "aws_instance" "bastion-host" {
   count           = var.availability_zones_count
   ami             = data.aws_ami.ubuntu.id
   key_name        = aws_key_pair.awskeypair.key_name
 
   instance_type = "t2.micro"
-
-  network_interface {
-
-    network_interface_id = aws_network_interface.bastion-host[count.index].id
-    device_index         = 0
-  }
-
+  subnet_id = aws_subnet.public[count.index].id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  
   tags = {
     Name = "bastion-host-${count.index}"
     Type = "bastion-host"
